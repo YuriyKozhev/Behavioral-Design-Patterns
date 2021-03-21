2a843a24ac7c2ac9a45b3f133afce2c3b67eabfb
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-03-21 17:21:25 +0300

    update Originator with 2 content strings

--- Memento/originator.py
+++ Memento/originator.py
@@ -1,23 +1,31 @@
-from memento import *
+from concrete_memento import ConcreteMemento
+
 
 class Originator(object):
-    def __init__(self, content: str = "") -> None:
-        self.content = content
+    def __init__(self, content_a: str = "a", content_b: str = "b") -> None:
+        print(f'Creating originator with content_a = {content_a}, content_b = {content_b}')
+        self._content_a = content_a
+        self._content_b = content_b
 
-    def set_content(self, content) -> None:
-        print(f'Setting originator content with value = {content}')
-        self.content = content
+    def set_content(self, content_a: str, content_b: str) -> None:
+        print(f'Setting originator content_a = {content_a}, content_b = {content_b}')
+        self._content_a = content_a
+        self._content_b = content_b
 
-    def get_content(self) -> str:
-        return self.content
+    def get_content(self) -> tuple:
+        return (self._content_a, self._content_b)
     
-    def create_state(self) -> Memento:
-        print(f'Creating originator state with value = {self.content}')
-        return Memento(self.content)
+    def create_state(self) -> ConcreteMemento:
+        print(f'Creating originator state with content_a = {self._content_a}, content_b = {self._content_b}')
+        return ConcreteMemento(self._content_a, self._content_b)
     
-    def restore_state(self, memento: Memento):
-        print('Restoring originator state')
-        self.content = memento.get_saved_state()
+    def restore_state(self, memento: ConcreteMemento):
+        try:
+            print(f'Restoring originator state with name = {memento.get_state_name()}')
+            self._content_a, self._content_b = memento.get_state()
+        except:
+            return
+
 
 if __name__ == "main":
     originator = Originator()
