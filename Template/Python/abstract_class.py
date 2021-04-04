4a4c833bcd3ce0d63fc6b1617a7acf0883b83c4d
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-04-04 23:50:02 +0300

    update abstract class

--- Template/Python/abstract_class.py
+++ Template/Python/abstract_class.py
@@ -2,15 +2,15 @@ from abc import ABC, abstractmethod
 
 class AbstractClass(ABC):
     def template_method(self) -> None:
-        self.primitive_operation_1()
-        self.primitive_operation_2()
+        self._primitive_operation_1()
+        self._primitive_operation_2()
 
     @abstractmethod
-    def primitive_operation_1(self) -> None:
+    def _primitive_operation_1(self) -> None:
         pass
 
     @abstractmethod
-    def primitive_operation_2(self) -> None:
+    def _primitive_operation_1(self) -> None:
         pass
     
 
