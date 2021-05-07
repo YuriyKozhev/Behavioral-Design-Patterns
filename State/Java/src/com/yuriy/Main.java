47f8db4b100ec06d72582bb17644f5cd6d55c69a
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-08 02:13:03 +0300

    add example of changing state of context

--- State/Java/src/com/yuriy/Main.java
+++ State/Java/src/com/yuriy/Main.java
@@ -3,6 +3,15 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
+        ConcreteStateA stateA = new ConcreteStateA();
+        ConcreteStateB stateB = new ConcreteStateB();
 
+        Context context = new Context();
+
+        context.setState(stateA);
+        context.request();
+
+        context.setState(stateB);
+        context.request();
     }
 }
