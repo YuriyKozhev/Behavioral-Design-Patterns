b1ae1915de24a4a7bc6c5957d7b72b650ccf8b67
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-08 13:07:37 +0300

    add example of iterating through ArrayCollection

--- Iterator/Java/src/com/yuriy/Main.java
+++ Iterator/Java/src/com/yuriy/Main.java
@@ -1,17 +1,30 @@
 package com.yuriy;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        Collection<Integer> collection = new ListCollection<>(List.of(1, 2, 3));
+        Collection<Integer> listCollection = new ListCollection<>(List.of(1, 2, 3));
 
-        Iterator<Integer> iterator = collection.createIterator();
+        Iterator<Integer> listIterator = listCollection.createIterator();
 
-        while (iterator.hasNext()) {
-            System.out.println(iterator.current());
-            iterator.next();
+        System.out.println("Forward iterating through list");
+        while (listIterator.hasNext()) {
+            System.out.println(listIterator.current());
+            listIterator.next();
+        }
+
+        Integer[] array = {1, 2, 3};
+        Collection<Integer> arrayCollection = new ArrayCollection<>(array);
+
+        Iterator<Integer> arrayIterator = arrayCollection.createIterator();
+
+        System.out.println("Backward iterating through array");
+        while (arrayIterator.hasNext()) {
+            System.out.println(arrayIterator.current());
+            arrayIterator.next();
         }
     }
 }
