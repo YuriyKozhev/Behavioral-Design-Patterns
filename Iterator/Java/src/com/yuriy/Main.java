a3532551d831fcbd4522fdcdc787d2b34ed3ae35
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-08 13:01:57 +0300

    add example of iterating through ListCollection

--- Iterator/Java/src/com/yuriy/Main.java
+++ Iterator/Java/src/com/yuriy/Main.java
@@ -1,8 +1,17 @@
 package com.yuriy;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Collection<Integer> collection = new ListCollection<>(List.of(1, 2, 3));
+
+        Iterator<Integer> iterator = collection.createIterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.current());
+            iterator.next();
+        }
     }
 }
