48f0fc8582418d5b4bc4070eb35301d4a6af456d
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-05-09 11:47:13 +0300

    create two concrete classes

---
+++ Template/Java/src/com/yuriy/ConcreteClassA.java
@@ -0,0 +1,13 @@
+package com.yuriy;
+
+public class ConcreteClassA extends AbstractClass {
+    @Override
+    void primitiveOperation1() {
+        System.out.println("Implementing operation 1 in A way");
+    }
+
+    @Override
+    void primitiveOperation2() {
+        System.out.println("Implementing operation 2 in A way");
+    }
+}
---
+++ Template/Java/src/com/yuriy/ConcreteClassB.java
@@ -0,0 +1,13 @@
+package com.yuriy;
+
+public class ConcreteClassB extends AbstractClass {
+    @Override
+    void primitiveOperation1() {
+        System.out.println("Implementing operation 1 in B way");
+    }
+
+    @Override
+    void primitiveOperation2() {
+        System.out.println("Implementing operation 2 in B way");
+    }
+}
