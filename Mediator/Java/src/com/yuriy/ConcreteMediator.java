de172aae940d1334d1231db3ae80fe3525f487c6
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-13 14:47:30 +0300

    implement mediate method for ConcreteMediator class

--- Mediator/Java/src/com/yuriy/ConcreteMediator.java
+++ Mediator/Java/src/com/yuriy/ConcreteMediator.java
@@ -1,8 +1,12 @@
 package com.yuriy;
 
 public class ConcreteMediator implements Mediator {
+    ConcreteColleagueA colleagueA = new ConcreteColleagueA(this);
+    ConcreteColleagueB colleagueB = new ConcreteColleagueB(this);
+
     @Override
     public void mediate(Colleague colleague) {
-
+        if (colleague == colleagueA && colleagueA.getValueA() > 1)
+            colleagueB.setValueB(1);
     }
 }
