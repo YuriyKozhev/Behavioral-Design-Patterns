3accec60ea59a09c90580035a3974bd0cdd00f90
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-13 12:46:28 +0300

    change Subject interface to abstract class with attach, detach and inform methods implementations

--- Observer/Java/src/com/yuriy/Subject.java
+++ Observer/Java/src/com/yuriy/Subject.java
@@ -1,7 +1,20 @@
 package com.yuriy;
 
-public interface Subject {
-    void attach(Observer obs);
-    void detach(Observer obs);
-    void inform();
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Subject {
+    private List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer obs) {
+        observers.add(obs);
+    }
+
+    public void detach(Observer obs) {
+        observers.remove(obs);
+    }
+
+    public void inform() {
+        observers.forEach(Observer::update);
+    }
 }
