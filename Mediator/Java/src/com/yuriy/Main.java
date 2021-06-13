c7b445a337e424ccbe77e4f2f4f0c2d170b52dbf
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-13 14:52:43 +0300

    add example of using Mediator

--- Mediator/Java/src/com/yuriy/Main.java
+++ Mediator/Java/src/com/yuriy/Main.java
@@ -3,6 +3,13 @@ package com.yuriy;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ConcreteMediator mediator = new ConcreteMediator();
+        ConcreteColleagueA colleagueA = new ConcreteColleagueA(mediator);
+        ConcreteColleagueB colleagueB = new ConcreteColleagueB(mediator);
+        mediator.setColleagueA(colleagueA);
+        mediator.setColleagueB(colleagueB);
+        System.out.println(colleagueB.getValueB());
+        colleagueA.setValueA(10);
+        System.out.println(colleagueB.getValueB());
     }
 }
