184479db51862d1e46ce270286e1951a609b0286
Юрий Кожев <yuriy.kozhev@gmail.com>
2021-06-14 12:34:40 +0300

    make ConcreteElementA and ConcreteElementB implement Element interface

--- Visitor/Java/src/com/yuriy/ConcreteElementA.java
+++ Visitor/Java/src/com/yuriy/ConcreteElementA.java
@@ -1,4 +1,8 @@
 package com.yuriy;
 
-public class ConcreteElementA {
+public class ConcreteElementA implements Element {
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
 }
--- Visitor/Java/src/com/yuriy/ConcreteElementB.java
+++ Visitor/Java/src/com/yuriy/ConcreteElementB.java
@@ -1,4 +1,8 @@
 package com.yuriy;
 
-public class ConcreteElementB {
+public class ConcreteElementB implements Element {
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
 }
